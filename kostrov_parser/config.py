68a3012a46607278bd3bd97b84fc76f5d1c881c8
Maxim Vasilev <Maxim.Vasilev@jetbrains.com>
2021-09-19 21:37:12 +0300

    Fix typo in TELEGRAM_CHAT_ID env parameter

--- kostrov_parser/config.py
+++ kostrov_parser/config.py
@@ -5,4 +5,4 @@ from dotenv import load_dotenv
 load_dotenv()
 
 TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
-TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_IT")
+TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
